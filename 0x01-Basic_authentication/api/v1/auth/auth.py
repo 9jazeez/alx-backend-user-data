259128fd0fd1bae91aeba1f1d07058f392d9a5b9
Lawal Azeez <9jazeez@gmail.com>
2022-05-18 11:42:59 +0000

    require auth modified

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,8 +12,19 @@ class Auth():
 	def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
 		""" A public method for authentication 
 		Returns
-		bool: False """
-		return False
+		bool: False or True """
+		if path is None or excluded_paths is None or not len(excluded_paths):
+			return True
+		if path[-1] != '/':
+			path += '/'
+		for val in excluded_paths:
+			if val.endswith('*'):
+				if path.startswith(val[:1]):
+					return False
+		if path in excluded_paths:
+			return False
+		else:
+			return True
 
 	def authorization_header(self, request=None) -> str:
 		""" auth using flask requst object """
