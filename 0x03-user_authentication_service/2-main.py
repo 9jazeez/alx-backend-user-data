1403c33264d834d24a070a4f6134fe7ea5c9494e
Lawal Azeez <9jazeez@gmail.com>
2022-05-24 13:35:22 +0000

    Find user

---
+++ 0x03-user_authentication_service/2-main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+from db import DB
+from user import User
+
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+
+my_db = DB()
+
+user = my_db.add_user("test@test.com", "PwdHashed")
+user2 = my_db.add_user("myemail@check.com", "WEqqESW")
+print(user2.id)
+
+find_user = my_db.find_user_by(email="test@test.com")
+print(find_user.id)
+
+try:
+    find_user = my_db.find_user_by(email="myem@check.com")
+    print(find_user.id)
+except NoResultFound:
+    print("Not found")
+
+try:
+    find_user = my_db.find_user_by(email="myemail@check.com")
+    print(find_user.id)
+except NoResultFound:
+    print("Not found")
+
+try:
+    find_user = my_db.find_user_by(no_email="test@test.com")
+    print(find_user.id)
+except InvalidRequestError:
+    print("Invalid")        
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,9 +5,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
-from user import Base
-from user import User
+from user import Base, User
 
 
 class DB:
@@ -17,7 +18,8 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db",
+                                     echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -37,3 +39,14 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs):
+        """Find a user method """
+        for name, val in kwargs.items():
+            if name in vars(User):
+                query = self._session.query(User).filter_by(email=val).first()
+                if query is None:
+                    raise NoResultFound
+                return query
+            else:
+                raise InvalidRequestError
