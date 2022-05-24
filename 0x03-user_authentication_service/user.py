9e8e19d81fcc9495213cfa523879be4ac506b842
Lawal Azeez <9jazeez@gmail.com>
2022-05-24 10:09:06 +0000

    create user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,6 +4,7 @@ named user using sqlalchemy declarative mapping. """
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
+from sqlalchemy import Sequence
 
 
 Base = declarative_base()
@@ -14,11 +15,17 @@ class User(Base):
     declarative class """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer,Sequence('user_id_seq'), primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __init__(self, email, hashed_password):
+        """ constructor """
+        self.email = email
+        self.hashed_password = hashed_password
+
 
     def __rpre__(self):
         """ Class formal description """
